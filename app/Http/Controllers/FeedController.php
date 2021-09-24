e7a311c888e9616c8752867345b007dec850dded
WispX <wisp-x@qq.com>
2021-09-24 17:44:30 +0800

    :sparkles: 改进订阅搜索

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -13,7 +13,8 @@ class FeedController extends Controller
     {
         $s = $request->input('s');
         $feeds = Feed::query()->when($s, function (Builder $builder, $s) {
-            $builder->where('title', 'like', "%{$s}%");
+            $fields = "concat(IFNULL(`title`,''),IFNULL(`author`,''),IFNULL(`link`,''),IFNULL(`desc`,''))";
+            $builder->whereRaw("{$fields} like ?", ["%{$s}%"]);
         })->orderBy('created_at', 'desc')->paginate();
         $total_blog = $feeds->total();
         return view('layouts.feeds.list', compact('feeds', 's', 'total_blog'));
@@ -23,7 +24,8 @@ class FeedController extends Controller
     {
         $s = $request->input('s');
         $blogs = Blog::query()->when($s, function (Builder $builder, $s) {
-            $builder->where('name', 'like', "%{$s}%");
+            $fields = "concat(IFNULL(`name`,''),IFNULL(`email`,''),IFNULL(`link`,''),IFNULL(`message`,''))";
+            $builder->whereRaw("{$fields} like ?", ["%{$s}%"]);
         })->where('status', 1)->whereNull('feed_link')->paginate();
         $total_blog = $blogs->total();
         return view('layouts.feeds.blogs', compact('blogs', 's', 'total_blog'));
