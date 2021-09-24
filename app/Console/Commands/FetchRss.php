b787ddf566988ef957b7223e0586bd06d2688f91
WispX <wisp-x@qq.com>
2021-09-24 13:14:33 +0800

    :sparkles: 改进抓取 rss 方法

--- app/Console/Commands/FetchRss.php
+++ app/Console/Commands/FetchRss.php
@@ -47,47 +47,41 @@ class FetchRss extends Command
      */
     public function handle()
     {
-        Blog::query()->where('status', 1)->whereNotNull('feed_link')->chunk(10, function ($blogs) {
-            foreach ($blogs as $blog) {
-                $feed = new SimplePie();
-                $feed->set_feed_url($blog->feed_link);
-                $feed->set_useragent($this::$useragent);
-                $feed->init();
-                $feed->enable_cache(true);
-                $feed->set_cache_duration(300);
-                $feed->handle_content_type();
-                if (!$feed->error) {
-                    // 清空 blog 关联的所有 feed
-                    Blog::find($blog->id)->feeds()->delete();
-
-                    $data = [];
-                    // 抓取 5 条 feed
-                    foreach ($feed->get_items(0, 5) as $item) {
-                        $tmp = [
-                            'title' => $item->get_title(),
-                            'author' => $item->get_author()->get_name(),
-                            'link' => $item->get_permalink(),
-                            'desc' => $item->get_description(),
-                            'created_at' => $item->get_date('Y-m-d H:i:s'),
-                            'updated_at' => $item->get_updated_date('Y-m-d H:i:s'),
-                        ];
-                        array_push($data, new Feed($tmp));
+        try {
+            Blog::query()->where('status', 1)->with('feeds')->whereNotNull('feed_link')->chunk(10, function ($blogs) {
+                /** @var Blog $blog */
+                foreach ($blogs as $blog) {
+                    $feed = new SimplePie();
+                    $feed->set_cache_location(storage_path('app/cache'));
+                    $feed->set_feed_url($blog->feed_link);
+                    $feed->set_useragent($this::$useragent);
+                    $feed->init();
+                    $feed->enable_cache(true);
+                    $feed->set_cache_duration(300);
+                    $feed->handle_content_type();
+                    if (!$feed->error) {
+                        foreach ($feed->get_items(0, 30) as $item) {
+                            Feed::query()->updateOrInsert([
+                                'blog_id' => $blog->id,
+                                'link' => $item->get_permalink(),
+                            ], [
+                                'title' => $item->get_title(),
+                                'author' => $item->get_author()->get_name(),
+                                'desc' => $item->get_description(),
+                                'created_at' => $item->get_date('Y-m-d H:i:s'),
+                                'updated_at' => $item->get_updated_date('Y-m-d H:i:s'),
+                            ]);
+                        }
+                        $blog->feed_status = 1;
+                    } else {
+                        $this->info("fetch rss ".$blog->feed_link." failed: ".$feed->error.PHP_EOL);
+                        $blog->feed_status = 2;
                     }
-                    // 重新插进入 feed
-                    if (!Blog::find($blog->id)->feeds()->saveMany($data)) {
-                        DB::rollBack();
-                    }
-                    // 修改博客 feed 状态
-                    if (!Blog::find($blog->id)->update(['feed_status' => 1])) {
-                        DB::rollBack();
-                    }
-                } else {
-                    echo("fetch rss ".$blog->feed_link." failed: ".$feed->error.PHP_EOL);
-                    if (!Blog::find($blog->id)->update(['feed_status' => 2])) {
-                        DB::rollBack();
-                    }
+                    $blog->save();
                 }
-            }
-        });
+            });
+        } catch (\Throwable $e) {
+            $this->error("抓取异常，file: {$e->getFile()}, line: {$e->getLine()}, msg: {$e->getMessage()}");
+        }
     }
 }
