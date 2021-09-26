b36a63423aef60bdd94eddbbbcf1af3d0f23dec9
WispX <wisp-x@qq.com>
2021-09-26 13:19:38 +0800

    :sparkles: 改进 feeds 接口

--- app/Http/Controllers/Api/V1/BlogController.php
+++ app/Http/Controllers/Api/V1/BlogController.php
@@ -60,7 +60,7 @@ class BlogController extends Controller
             $blog = $feed->blog;
             $feed->avatar = $blog->avatar;
             $feed->email_hash = md5(strtolower(trim($blog->email)));
-            $feed->setVisible(['id', 'title', 'author', 'avatar', 'email_hash', 'link', 'desc']);
+            $feed->setVisible(['id', 'title', 'author', 'avatar', 'email', 'link', 'desc']);
         });
         return $this->success('success', $feeds);
     }
